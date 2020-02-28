f1d5a8ac7f45b97ec061ea89f7ceb29b1b693640
Santiagotm921 <43409794+Santiagotm921@users.noreply.github.com>
2020-02-28 18:48:06 -0500

    The change made by the black button on the page has been controlled, when the original color changes its color by the screen size, the black color can replicate this action. In advertising, it has been changed to match the background black, taking into account that classes have had to be added to make these modifications in javascript. Also, a menu has been created in which you can find several links which the user may be interested in, but the footer of the page has not yet been added.

--- code/scripts/changeColor.js
+++ code/scripts/changeColor.js
@@ -7,47 +7,55 @@ var blue = "#0098f0";
 var breackpoint = 930;
 var ancho = window.innerWidth;
 
-//Class
-var principal = document.querySelector('.menu');
-var characteristics = document.querySelector('.characteristics');
-var menu = document.querySelector('.menu__container');
-var register = document.querySelector('.register');
-var hamburguer = document.querySelector('.mainnav');
-var bannerTexts = document.querySelector('.bannershoe__texts');
-
-var btnGiant = document.querySelector('.bannershoe__button');
-
-var h1 = document.querySelectorAll('h1');
-var publiShoes = document.querySelectorAll('.publicity__shoe');
-var publiTexts = document.querySelectorAll('.publicity__text');
-
-
-
 //Buttons
 var btnRed = document.querySelector('.buttons--red');
 var btnWhite = document.querySelector('.buttons--white');
 var btnBlack = document.querySelector('.buttons--black');
 var btnBlue = document.querySelector('.buttons--blue');
 
+//Class
+var principal = document.querySelector('.menu');
+var characteristics = document.querySelector('.characteristics');
+var register = document.querySelector('.register');
+var hamburguer = document.querySelector('.mainnav');
+var bannerTexts = document.querySelector('.bannershoe__texts');
+var btnGiant = document.querySelector('.bannershoe__button');
+
+
+var h1 = document.querySelectorAll('h1');
+var publiTexts = document.querySelectorAll('.publicity__text');
+
+
+/*CHANGE CLASSES */
+
+var menu = document.querySelector('.menu__container');
+var menuBlack = document.querySelector('.menu__container--black');
+
+var publiShoes = document.querySelectorAll('.publicity__shoe');
+var publiShoesBlack = document.querySelectorAll('.publicity__shoe--black');
+
+
+/*CHANGE CLASSES */
+
 var handleBlack = function () {
+
     principal.style.background = black;
     characteristics.style.background = black;
-    hamburguer.style.background = red;
-    menu.style.background = red;
     register.style.background = black;
 
-
+    
     for (let i = 0; i < publiShoes.length; i++) {
         const shoe = publiShoes[i];
-        shoe.style.background = red;
+        shoe.classList.toggle('publicity__shoe--black');
     }
 
     for (let i = 0; i < publiTexts.length; i++) {
         const textsShoes = publiTexts[i];
-        textsShoes.style.color = white;
+        textsShoes.classList.toggle('publicity__shoe--black');
+    } 
 
-    }
-
+    /*CHANGE CLASSES */
+    menu.classList.toggle('menu__container--black');
 }
 
 var handleWhite = function () {
@@ -63,7 +71,6 @@ var handleWhite = function () {
         shoe.style.background = red;
         shoe.style.color = white;
     }
-
 }
 
 var handleRed = function () {
