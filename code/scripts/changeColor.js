b5bdbc0743332e095241641d00dff00bedfbc256
Santiagotm921 <43409794+Santiagotm921@users.noreply.github.com>
2020-03-04 07:35:45 -0500

    Se ha cambiado el toggle, en lugar de este, se ha incluido las operaciones de add y remove, para tener mayor control a la hora de agregar contenido o quitarlos en determinado momento, cuando la pantalla cambia de estado de color, todo esto en el archivo ChangeColor.js. Se ha añadido un slider para las imagenes de las caractersiticas del producto cuando la pantalla llega a su punto de quiebre. Cuando se realiza un cambio de color en la pagina, se notifica al usuario acerca de este cambio con un modal, visto en clase, se ha incluido en el archivo modal.js.

--- code/scripts/changeColor.js
+++ code/scripts/changeColor.js
@@ -2,7 +2,10 @@
 var black = "#000"
 var red = "#aa0400";
 var white = "#fff";
-var blue = "#0098f0";
+var yellow = "#f9be24";
+var rose = "#fc0085";
+var none = "none";
+var flex = "flex";
 
 var breackpoint = 930;
 var ancho = window.innerWidth;
@@ -11,18 +14,27 @@ var ancho = window.innerWidth;
 var btnRed = document.querySelector('.buttons--red');
 var btnWhite = document.querySelector('.buttons--white');
 var btnBlack = document.querySelector('.buttons--black');
-var btnBlue = document.querySelector('.buttons--blue');
+var btnYellow = document.querySelector('.buttons--yellow');
+//Buttons
 
 //Class
 var principal = document.querySelector('.menu');
+var principalBlack = document.querySelector('.menu--black');
+var principalWhite = document.querySelector('.menu--white');
+
+
 var characteristics = document.querySelector('.characteristics');
-var register = document.querySelector('.register');
+var characteristicsBlack = document.querySelector('.characteristics--black');
+var characteristicsWhite = document.querySelector('.characteristics--white');
 var hamburguer = document.querySelector('.mainnav');
+var hamburguerWhite = document.querySelector('.mainnav--white');
+
 var bannerTexts = document.querySelector('.bannershoe__texts');
+var bannerTextsWhite = document.querySelector('.bannershoe__texts--white');
+
 var btnGiant = document.querySelector('.bannershoe__button');
+var btnGiantWhite = document.querySelector('.bannershoe__button--white');
 
-
-var h1 = document.querySelectorAll('h1');
 var publiTexts = document.querySelectorAll('.publicity__text');
 
 
@@ -30,55 +42,138 @@ var publiTexts = document.querySelectorAll('.publicity__text');
 
 var menu = document.querySelector('.menu__container');
 var menuBlack = document.querySelector('.menu__container--black');
+var menuWhite = document.querySelector('.menu__container--white');
+
+var changeColor = document.querySelector('.changeColor');
+var changeColorBlack = document.querySelector('.changeColor--black');
 
 var publiShoes = document.querySelectorAll('.publicity__shoe');
 var publiShoesBlack = document.querySelectorAll('.publicity__shoe--black');
+var publiShoesWhite = document.querySelectorAll('.publicity__shoe--white');
 
+var register = document.querySelector('.register');
+var registerBlack = document.querySelector('.register--black');
+var registerWhite = document.querySelector('.register--white');
+
+var shoeColors = document.querySelector('.changeColor__shoe');
+var shoeColorsWhite = document.querySelector('.changeColor__shoe--white');
+var shoeColorsBlack = document.querySelector('.changeColor__shoe--black');
+var shoeColorsYellow = document.querySelector('.changeColor__shoe--yellow');
 
 /*CHANGE CLASSES */
 
 var handleBlack = function () {
 
-    principal.style.background = black;
-    characteristics.style.background = black;
-    register.style.background = black;
-
-    
     for (let i = 0; i < publiShoes.length; i++) {
         const shoe = publiShoes[i];
-        shoe.classList.toggle('publicity__shoe--black');
+        shoe.classList.add('publicity__shoe--black');
     }
 
     for (let i = 0; i < publiTexts.length; i++) {
         const textsShoes = publiTexts[i];
-        textsShoes.classList.toggle('publicity__shoe--black');
-    } 
+        textsShoes.classList.add('publicity__shoe--black');
+    }
 
     /*CHANGE CLASSES */
-    menu.classList.toggle('menu__container--black');
+    menu.classList.add('menu__container--black');
+    characteristics.classList.add('characteristics--black');
+    changeColor.classList.add('changeColor--black');
+    principal.classList.add('menu--black');
+    register.classList.add('register--black');
+
+    //CAMBIAR ZAPATO POR OTRO ------- CONDIONAL------
+    shoeColors.classList.add('changeColor__shoe--black');
+    shoeColorsBlack.style.display = flex;
+
+    if (shoeColorsBlack.style.display = flex) {
+
+        shoeColors.style.display = none;
+        shoeColorsWhite.style.display = none;
+        shoeColorsYellow.style.display = none;
+    }
+
+    removeWhite();
 }
 
 var handleWhite = function () {
-    principal.style.background = white;
-    characteristics.style.background = white;
-    hamburguer.style.background = red;
-    menu.style.background = red;
-    bannerTexts.style.color = red;
-    btnGiant.style.background = red;
 
     for (let i = 0; i < publiShoes.length; i++) {
         const shoe = publiShoes[i];
-        shoe.style.background = red;
-        shoe.style.color = white;
+        shoe.classList.add('publicity__shoe--white');
     }
+
+    register.classList.add('register--white');
+    principal.classList.add('menu--white');
+    characteristics.classList.add('characteristics--white');
+    hamburguer.classList.add('mainnav--white')
+    bannerTexts.classList.add('bannershoe__texts--white');
+    btnGiant.classList.add('bannershoe__button--white');
+    menu.classList.add('menu__container--white');
+
+    //CAMBIAR ZAPATO POR OTRO ------- CONDIONAL------
+
+    shoeColors.classList.add('changeColor__shoe--white');
+    shoeColorsWhite.style.display = flex;
+
+    if (shoeColorsWhite.style.display = flex) {
+
+        shoeColors.style.display = none;
+        shoeColorsBlack.style.display = none;
+        shoeColorsYellow.style.display = none;
+    }
+
+    removeBlack();
+
+}
+
+var handleYellow = function () {
+
+    //CAMBIAR ZAPATO POR OTRO ------- CONDIONAL------
+    shoeColors.classList.add('changeColor__shoe--yellow');
+    shoeColorsYellow.style.display = flex;
+
+    if (shoeColorsYellow.style.display = flex) {
+
+        shoeColors.style.display = none;
+        shoeColorsBlack.style.display = none;
+        shoeColorsWhite.style.display = none;
+    }
+
+    removeBlack();
+    removeWhite();
+
 }
 
 var handleRed = function () {
     window.location.reload();
 }
 
+var removeBlack = function () {
+    /*publiShoes.classList.remove('publicity__shoe--black');
+    publiTexts.classList.remove('publicity__shoe--black'); */
+    menu.classList.remove('menu__container--black');
+    characteristics.classList.remove('characteristics--black');
+    changeColor.classList.remove('changeColor--black');
+    principal.classList.remove('menu--black');
+    register.classList.remove('register--black');
+    shoeColors.classList.remove('changeColor__shoe--black');
+}
 
-/*btnBlue.addEventListener('click', handleBlue);*/
+var removeWhite = function () {
+
+    register.classList.remove('register--white');
+    principal.classList.remove('menu--white');
+    characteristics.classList.remove('characteristics--white');
+    hamburguer.classList.remove('mainnav--white')
+    bannerTexts.classList.remove('bannershoe__texts--white');
+    btnGiant.classList.remove('bannershoe__button--white');
+    menu.classList.remove('menu__container--white');
+    shoeColors.classList.remove('changeColor__shoe--white');
+}
+
+btnYellow.addEventListener('click', handleYellow);
 btnRed.addEventListener('click', handleRed);
 btnBlack.addEventListener('click', handleBlack);
 btnWhite.addEventListener('click', handleWhite);
+
+
